1609b13762821f46297a62e386ee611fb7a115a6
al3xdiaz <alexleonel96@hotmail.com>
2023-08-13 21:19:45 -0600

    [fix] creating ssh key ansible if not exist

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@ alias ll="ls -l"
 alias la="ls -A"
 alias l="ls -CF"
 alias lll="ls -l | less"
-cp /home/.ssh/ansible /root/.ssh/ansible
-chmod 600 /root/.ssh/ansible
-cp /home/.ssh/ansible.pub /root/.ssh/ansible.pub
-
-if [ ! -f /root/.ssh/ansible ]; then
-    ssh-keygen -b 3072 -t rsa -f /root/.ssh/ansible -q -N '' -C ansible
+if [ ! -f /home/user_keys/ansible ]; then
+    ssh-keygen -b 3072 -t rsa -f /home/user_keys/ansible -q -N '' -C ansible
+else
+    cp /home/user_keys/ansible /root/.ssh/ansible
+    chmod 600 /root/.ssh/ansible
+    cp /home/user_keys/ansible.pub /root/.ssh/ansible.pub
 fi
 chown root /root/.ssh/*
 exec $@
